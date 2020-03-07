3a7faf233fe8ffd002c8d09e3df150d84b17491e
AnsaryMarecar <an.marecar@gmail.com>
2020-03-07 21:40:39 +0100

    (use case : Le système doit avoir une accès vers le level suivant) Ajout d'un script pour la sortie du level avec collision entre joueur et la sortie Ajout d'un script au niveau de Player

---
+++ Assets/Scripts/Exit.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SDD.Events;
+
+public class Exit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // When collision between these object and Player
+    private void OnTriggerEnter2D(Collider2D collision){
+        if (collision.gameObject.GetComponent<Player>() != null){
+            EventManager.Instance.Raise(new AskToGoToNextLevelEvent());
+        }
+    }
+}
